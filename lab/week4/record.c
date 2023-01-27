562ec75841d29d6e9eb139d4a1089c8ff87d67e8
shaswat-satyam <70892928+shaswat-satyam@users.noreply.github.com>
2023-01-27 15:34:31 +0000

    week 4

---
+++ lab/week4/record.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int m,n;
+
+    int id,salary;
+    char name[30];
+
+    FILE* fl;
+    fl = fopen("file.txt","r+");
+
+
+    printf("Enter the number of managers and workers: ");
+    scanf("%d %d",&m,&n);
+
+
+    printf("Enter the details of the managers :\n|id|salary|name|\n");
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d %d %s",&id, &salary, name);
+        fprintf(fl,"%d %d %s\n",id,salary,name);
+    }
+
+    fprintf(fl,"\n");
+
+    printf("Enter the details of the workers :\n|id|salary|name|\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d %s",&id, &salary, name);
+        fprintf(fl,"%d %d %s\n",id,salary,name);
+    }
+    
+    fclose(fl);
+    return 0;
+}
