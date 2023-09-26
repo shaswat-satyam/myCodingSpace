3a74f9cd648423b16661d8ba8b2c4b2ef1289c85
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2023-09-26 16:21:58 +0000

    Updated the linkedlist

--- DS/linkedlist.cpp
+++ DS/linkedlist.cpp
@@ -1,129 +1,147 @@
-#include <iostream>
-using namespace std;
+        #include <iostream>
+        using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node* next;
-    
-        Node(int value){
-            data = value;
-            next = nullptr;
-        }
-};
-
-class linkedList{
-    Node* head;
-    Node* tail;
-
-    public:
-        linkedList(){
-            head = nullptr;
-            tail = nullptr;
-        }
-
-        void display(){
-            Node* curr = head;
-            int idx = 0;
-            cout << "Index: ";
-            while(curr){
-                cout << idx++ << "    ";
-                curr = curr->next;
-            }
-            cout << endl << "Value: ";
-            curr = head;
-            while(curr){
-                if(curr->next){
-                    cout << curr->data << " -> ";
-                }
-                else{
-                    cout << curr->data;
-                }
-                curr = curr->next;
-            }
-            cout << endl << endl;
-        }
-
-        void append(int data){
-            Node* newNode = new Node(data);
-            if(tail){
-                tail->next = newNode;
-                tail = tail -> next;
-            }
-            else{
-                tail = newNode;
-                head = newNode;
-            }
-        }
-
-        void prepend(int data){
-            Node* newNode = new Node(data);
-            if(head){
-                newNode->next = head;
-                head = newNode;
-            }
-            else{
-                tail = newNode;
-                head = newNode;
-            }
-        }
-
-        void insert(int data, int idx){
-            Node* curr = head;
-            Node* newNode = new Node(data);
+        class Node{
+            public:
+                int data;
+                Node* next;
             
-            curr = head;
-            while(--idx){
-                curr = curr->next;
-                if(!curr){
-                    cout << "Index is out of bounds!" << endl;
-                    return;
+                Node(int value){
+                    data = value;
+                    next = nullptr;
                 }
-            }
-            newNode->next = curr->next;
-            curr->next = newNode;
+        };
+
+        class linkedList{
+            Node* head;
+            Node* tail;
+
+            public:
+                linkedList(){
+                    head = nullptr;
+                    tail = nullptr;
+                }
+
+                void display(){
+                    Node* curr = head;
+                    int idx = 0;
+                    cout << "Index: ";
+                    while(curr){
+                        cout << idx++ << "    ";
+                        curr = curr->next;
+                    }
+                    cout << endl << "Value: ";
+                    curr = head;
+                    while(curr){
+                        if(curr->next){
+                            cout << curr->data << " -> ";
+                        }
+                        else{
+                            cout << curr->data;
+                        }
+                        curr = curr->next;
+                    }
+                    cout << endl << endl;
+                }
+
+                void append(int data){
+                    Node* newNode = new Node(data);
+                    if(tail){
+                        tail->next = newNode;
+                        tail = tail -> next;
+                    }
+                    else{
+                        tail = newNode;
+                        head = newNode;
+                    }
+                }
+
+                void prepend(int data){
+                    Node* newNode = new Node(data);
+                    if(head){
+                        newNode->next = head;
+                        head = newNode;
+                    }
+                    else{
+                        tail = newNode;
+                        head = newNode;
+                    }
+                }
+
+                void insert(int data, int idx){
+                    Node* curr = head;
+                    Node* newNode = new Node(data);
+                    
+                    curr = head;
+                    while(--idx){
+                        curr = curr->next;
+                        if(!curr){
+                            cout << "Index is out of bounds!" << endl;
+                            return;
+                        }
+                    }
+                    newNode->next = curr->next;
+                    curr->next = newNode;
+                }
+
+                void reverse(){
+                    Node * prev = nullptr;
+                    Node * curr = head;
+                    Node * next = nullptr;
+                    while (curr != NULL) {
+                    // Store next
+                        next = curr->next;
+                        // Reverse current node's pointer
+                        curr->next = prev;
+                        // Move pointers one position ahead.
+                        prev = curr;
+                        curr = next;
+                    }
+                    head = prev;
+                }
+
+                void remove(int idx){
+                    Node* curr = head;
+                    
+                    curr = head;
+                    while(--idx){
+                        curr = curr->next;
+                        if(!curr){
+                            cout << "Index is out of bounds!" << endl;
+                            return;
+                        }
+                    }
+                    curr->next = curr->next->next;  
+                }
+        };
+
+        int main(){
+            linkedList l1;
+            cout << "The Initial Linked List" << endl;
+            l1.append(1);
+            l1.append(2);
+            l1.append(3);
+            l1.append(4);
+            l1.append(5);    
+            l1.display();
+            
+            cout << "Appending 69 to the Linked List" << endl;
+            l1.append(69);
+            l1.display();
+            
+            cout << "Prepending 420 to the Linked List" << endl;
+            l1.prepend(420);
+            l1.display();
+
+            cout <<"Inserting 9 at position 3 in the Linked List" << endl;
+            l1.insert(9,3);
+            l1.display();
+
+            cout <<"Reversing the Linked List" << endl;
+            l1.reverse();
+            l1.display();
+
+            cout <<"Removing the element at 3 index Linked List" << endl;
+            l1.remove(3);
+            l1.display();
         }
-
-        void reverse(){
-            Node * prev = nullptr;
-            Node * curr = head;
-            Node * next = nullptr;
-            while (curr != NULL) {
-            // Store next
-                next = curr->next;
-                // Reverse current node's pointer
-                curr->next = prev;
-                // Move pointers one position ahead.
-                prev = curr;
-                curr = next;
-            }
-            head = prev;
-        }
-};
-
-int main(){
-    linkedList l1;
-    cout << "The Initial Linked List" << endl;
-    l1.append(1);
-    l1.append(2);
-    l1.append(3);
-    l1.append(4);
-    l1.append(5);    
-    l1.display();
-    
-    cout << "Appending 69 to the Linked List" << endl;
-    l1.append(69);
-    l1.display();
-    
-    cout << "Prepending 420 to the Linked List" << endl;
-    l1.prepend(420);
-    l1.display();
-
-    cout <<"Inserting 9 at position 3 in the Linked List" << endl;
-    l1.insert(9,3);
-    l1.display();
-
-    cout <<"Reversing the Linked List" << endl;
-    l1.reverse();
-    l1.display();
-}
