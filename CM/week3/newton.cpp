bea5b7334ab01f698143c159cd0c7cbce6f40edf
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2023-10-06 03:22:25 +0000

    Updated Newton Methods

--- CM/week3/newton.cpp
+++ CM/week3/newton.cpp
@@ -1,28 +1,39 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
-#include <bits/stdc++.h>
+// #include <conio.h>
+// #include <iomanip.h>
+
+#include <math.h>
 using namespace std;
 
-double func(double x){
-    return x*x*x  - 4*x - 9;
+
+float f(float x){
+    return x*x*x - 4*x -9;
 }
 
-double funcD(double x){
-    return 3*x*x - 4;
+float df (float x){
+    return 3*x*x -4;
 }
 
-void newtonRhapson(double x1){
-    double h = func(x1)/funcD(x1);
-    while( abs(h) >= 0.0001  ){
-        h = func(x1)/funcD(x1);
-        x1 = x1 - h;
+int main (){
+    int itr, maxitr, fixed;
+    float h,x0,x1,aerr;
+    
+    cout << "Enter x0,allowed error and maximum iterations" <<endl;
+    
+    cin >> x0 >> aerr >> maxitr;
+
+    
+    for(itr = 0 ;itr < maxitr; itr++ ){
+        h = f(x0)/df(x0);
+        cout << "Iteration no." <<  itr << ", X= "<< x0 << ", h = "<< h <<endl;
+        if (abs(h)< aerr){
+            cout << "After " << itr << " iterations, root = "<< x0 << endl;
+            return 0;
+        }
+        x0 = x0 - h;
     }
-    cout << "The value of the root is :" << x1 << endl;
-}
+    cout << "Iterations not sufficient or Solution does not converge!" << endl;
+    
+    return 1;
 
-int main() {
-    // Write C++ code here
-    double Xo = -20;
-    newtonRhapson(Xo);
-    return 0;
 }
